135666deadbd21e3a042be4639d7a415cd40b3bd
Larissa de Magalhães Caldeira <107259689+Larimcal@users.noreply.github.com>
2025-01-23 00:11:56 -0300

    Adicionando teste CT 7

--- Selenium/tafe_aula1/src/test/java/br/com/dbccompany/VerifySubscriptionInHomePageTest.java
+++ Selenium/tafe_aula1/src/test/java/br/com/dbccompany/VerifySubscriptionInHomePageTest.java
@@ -1,4 +1,35 @@
 package br.com.dbccompany;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class VerifySubscriptionInHomePageTest {
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @BeforeTest
+    public void abrirNavegador(){
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(40));
+        driver.get("https://www.automationexercise.com");
+        driver.manage().window().maximize();
+    }
+
+   /* @Test
+    public void
+
+*/
+
+
+
+    @AfterTest
+    public void finalizarNavegador(){
+        driver.quit();
+    }
 }
